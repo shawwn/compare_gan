4588dca041191c57a7ab9bc60ef6167d95b044d5
Shawn Presser <shawnpresser@gmail.com>
2020-04-08 12:48:54 -0700

    Minor

--- run.sh
+++ run.sh
@@ -3,4 +3,4 @@ set -ex
 export TPU_HOST=10.255.128.3
 export TPU_NAME=tpu-v3-256-euw4a-27
 export MODEL_DIR=gs://danbooru-euw4a/test/bigrun00/
-exec python3 wrapper.py compare_gan/main.py --use_tpu --tfds_data_dir 'gs://danbooru-euw4a/tensorflow_datasets/' --model_dir "${model_dir}" --gin_config "$@"
+exec python3 wrapper.py compare_gan/main.py --use_tpu --tfds_data_dir 'gs://danbooru-euw4a/tensorflow_datasets/' --model_dir "${MODEL_DIR}" --gin_config "$@"
