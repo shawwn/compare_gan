4829fdc8ec279832954afa051c43f5235d361144
Shawn Presser <shawnpresser@gmail.com>
2020-04-11 08:34:25 -0700

    Drop the TPU core count to 128

--- run.sh
+++ run.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -ex
 export TPU_HOST=10.255.128.3
-export TPU_NAME=tpu-v3-512-euw4a-27
+export TPU_NAME=tpu-v3-128-euw4a-27
 export MODEL_DIR=gs://danbooru-euw4a/test/bigrun04/
 #export DATASETS=gs://danbooru-euw4a/datasets/danbooru2019-s/danbooru2019-s-0*,gs://danbooru-euw4a/datasets/e621-s/e621-s-0*
 export DATASETS=gs://danbooru-euw4a/datasets/danbooru2019-s/danbooru2019-s-0*
