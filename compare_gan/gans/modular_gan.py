7bf4da4a8cbad8b7d53a95178fa00b5a8364e3e5
Shawn Presser <shawnpresser@gmail.com>
2020-04-20 18:59:51 -0700

    Make labels_var a local variable

--- compare_gan/gans/modular_gan.py
+++ compare_gan/gans/modular_gan.py
@@ -406,7 +406,7 @@ class ModularGAN(AbstractGAN):
         for label_file in label_files:
           all_labels.extend([int(x) for x in tf.io.gfile.GFile(label_file).read().splitlines()])
         self._dataset._all_labels = all_labels
-      v = tf.Variable(self._dataset._all_labels, name="labels_var", dtype=tf.int32, trainable=False)
+      v = tf.Variable(self._dataset._all_labels, name="labels_var", dtype=tf.int32, trainable=False, collections=[tf.GraphKeys.LOCAL_VARIABLES])
       logging.info("Returning real random labels %s", v)
       with tf.control_dependencies([v.initializer]):
         return tf.gather(v, tf.random_uniform(shape, 0, v.shape[0], dtype=tf.int32))
