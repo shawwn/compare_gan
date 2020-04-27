0df78c5524206b60f456648cf428954bf0e94d78
Shawn Presser <shawnpresser@gmail.com>
2020-04-27 18:02:26 +0000

    Add run_danbooru scripts

---
+++ run_danbooru512.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+set -ex
+#export TPU_HOST=${TPU_HOST:-10.255.128.3}
+export TPU_HOST=${TPU_HOST:-10.255.128.2}
+#export TPU_NAME="${TPU_NAME:-tpu-v3-128-euw4a-24}"
+export TPU_NAME="${TPU_NAME:-tpu-v3-256-euw4a-25}"
+export MODEL_DIR="${MODEL_DIR:-gs://darnbooru-euw4a/runs/bigrun38/}"
+export DATASETS=gs://danbooru-euw4a/datasets/danbooru2019-s/danbooru2019-s-0*
+export LABELS=""
+export NUM_CLASSES=1000
+exec python3 wrapper.py compare_gan/main.py --use_tpu --tfds_data_dir 'gs://darnbooru-euw4a/tensorflow_datasets/' --model_dir "${MODEL_DIR}" --gin_config example_configs/biggan_danbooru512.gin "$@"
