79b280aa9343ae3a6de5ba79024e684742fc39b9
Shawn Presser <shawnpresser@gmail.com>
2020-04-29 19:52:01 -0700

    Add timeouts to scripts

--- run_bigrun41.sh
+++ run_bigrun41.sh
@@ -6,6 +6,6 @@ export MODEL_DIR="${MODEL_DIR:-gs://darnbooru-euw4a/runs/bigrun41/}"
 export GIN_CONFIG="${GIN_CONFIG:-example_configs/bigrun41.gin}"
 export LOGDIR="${LOGDIR:-logs41.txt}"
 while true; do
-  python3 wrapper.py compare_gan/main.py --use_tpu --tfds_data_dir 'gs://danbooru-euw4a/tensorflow_datasets/' --model_dir "${MODEL_DIR}" --gin_config "$GIN_CONFIG" "$@" 2>&1 | tee -a "${LOGDIR}"
+  timeout --signal=SIGKILL 19h python3 wrapper.py compare_gan/main.py --use_tpu --tfds_data_dir 'gs://danbooru-euw4a/tensorflow_datasets/' --model_dir "${MODEL_DIR}" --gin_config "$GIN_CONFIG" "$@" 2>&1 | tee -a "${LOGDIR}"
   sleep 30
 done
