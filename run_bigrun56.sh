81879b22913a17c68504d12eabd8c2f2e24033ef
Shawn Presser <shawnpresser@gmail.com>
2020-05-01 15:47:36 -0700

    Rename runs

--- run_bigrun56.sh
+++ run_bigrun56.sh
@@ -3,9 +3,9 @@ set -ex
 #export TPU_HOST="${TPU_HOST:-10.255.128.3}"
 export TPU_HOST="${TPU_HOST:-10.255.128.2}"
 export TPU_NAME="${TPU_NAME:-tpu-v3-256-euw4a-28}"
-export MODEL_DIR="${MODEL_DIR:-gs://darnbooru-euw4a/runs/bigrun53/}"
-export GIN_CONFIG="${GIN_CONFIG:-example_configs/bigrun53.gin}"
-export LOG="${LOG:-logs53.txt}"
+export MODEL_DIR="${MODEL_DIR:-gs://darnbooru-euw4a/runs/bigrun56/}"
+export GIN_CONFIG="${GIN_CONFIG:-example_configs/bigrun56.gin}"
+export LOG="${LOG:-logs56.txt}"
 while true; do
   timeout --signal=SIGKILL 19h python3 wrapper.py compare_gan/main.py --use_tpu --tfds_data_dir 'gs://danbooru-euw4a/tensorflow_datasets/' --model_dir "${MODEL_DIR}" --gin_config "$GIN_CONFIG" "$@" 2>&1 | tee -a "${LOG}"
   sleep 30
--- run_bigrun57.sh
+++ run_bigrun57.sh
@@ -3,9 +3,9 @@ set -ex
 #export TPU_HOST="${TPU_HOST:-10.255.128.3}"
 export TPU_HOST="${TPU_HOST:-10.255.128.2}"
 export TPU_NAME="${TPU_NAME:-tpu-v3-128-euw4a-24}"
-export MODEL_DIR="${MODEL_DIR:-gs://darnbooru-euw4a/runs/bigrun54/}"
-export GIN_CONFIG="${GIN_CONFIG:-example_configs/bigrun54.gin}"
-export LOG="${LOG:-logs54.txt}"
+export MODEL_DIR="${MODEL_DIR:-gs://darnbooru-euw4a/runs/bigrun57/}"
+export GIN_CONFIG="${GIN_CONFIG:-example_configs/bigrun57.gin}"
+export LOG="${LOG:-logs57.txt}"
 while true; do
   timeout --signal=SIGKILL 19h python3 wrapper.py compare_gan/main.py --use_tpu --tfds_data_dir 'gs://danbooru-euw4a/tensorflow_datasets/' --model_dir "${MODEL_DIR}" --gin_config "$GIN_CONFIG" "$@" 2>&1 | tee -a "${LOG}"
   sleep 30
