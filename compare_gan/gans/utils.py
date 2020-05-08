b6682c5c4232e9cd7d6ea063ecc043da991751d5
Shawn Presser <shawnpresser@gmail.com>
2020-05-08 12:34:42 -0700

    WIP

--- compare_gan/gans/utils.py
+++ compare_gan/gans/utils.py
@@ -278,7 +278,12 @@ def tf_fftconv(in1, in2, mode="full"):
   result = cropped
   return result
 
-
+def tf_fftconv(a, b, mode):
+  a = tf.transpose(a, [0, 2, 3, 1])
+  b = tf.transpose(b, [2, 3, 0, 1])
+  r = tf.nn.conv2d(a, b, [1, 1, 1, 1], 'VALID')
+  r = tf.transpose(r, [0, 3, 1, 2])
+  return r
 
 from tensorflow.python.ops import array_ops, math_ops
 
@@ -372,11 +377,11 @@ def _i(x): return tf.transpose(x, [0,2,3,1])
 def _o(x): return tf.transpose(x, [0,3,1,2])
 
 def tf_similarity(images, **kws):
-  n, c, h, w = images.shape.as_list()
+  #n, c, h, w = images.shape.as_list()
   imgs1 = images
   imgs2 = tf.concat([imgs1[0:-1], [imgs1[-1]]], axis=0)
-  imgs2.set_shape([n, c, h, w])
-  #return tf_ssim_multiscale(imgs1, imgs2, **kws)
-  result = tf.image.ssim_multiscale(_i(imgs1), _i(imgs2), 1.0)
-  result = tf.stop_gradient(result)
-  return result
+  #imgs2.set_shape([n, c, h, w])
+  return tf_ssim_multiscale(imgs1, imgs2, **kws)
+  # result = tf.image.ssim_multiscale(_i(imgs1), _i(imgs2), 1.0)
+  # result = tf.stop_gradient(result)
+  # return result
