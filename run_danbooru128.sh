1ac90e6b65e90f71cd018fe84ac4b92311b1e156
Shawn Presser <shawnpresser@gmail.com>
2020-05-09 15:03:34 -0700

    Configure bigrun36 for dynamic vars

--- run_danbooru128.sh
+++ run_danbooru128.sh
@@ -4,7 +4,8 @@ export LD_LIBRARY_PATH="${LD_LIBRARY_PATH:-/tfk/lib}"
 #export TPU_HOST=10.255.128.3
 export TPU_HOST=${TPU_HOST:-10.255.128.2}
 export TPU_NAME="${TPU_NAME:-tpu-v3-128-euw4a-29}"
-printf '\033]2;%s\033\\' "bigrun36 ${TPU_NAME}" # set tmux title
+export TENSORFORK_RUN="tensorfork/tensorfork#1"
+printf '\033]2;%s\033\\' "bigrun36 ${TPU_NAME} @ ${TPU_HOST} (${TENSORFORK_RUN})" # set tmux title
 export MODEL_DIR="${MODEL_DIR:-gs://darnbooru-euw4a/runs/bigrun36c/}"
 export DATASETS=gs://danbooru-euw4a/datasets/danbooru2019-s/danbooru2019-s-0*
 export LABELS=""
