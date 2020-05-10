a4bcbdd8046d645a6ec2dbde57571adf9d475821
Shawn Presser <shawnpresser@gmail.com>
2020-05-10 10:33:15 -0700

    Add run_bigrun61.sh

---
+++ run_bigrun61.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+set -ex
+export LD_LIBRARY_PATH="${LD_LIBRARY_PATH:-/tfk/lib}"
+#export TPU_HOST=10.255.128.3
+export TPU_HOST=${TPU_HOST:-10.255.128.2}
+export TPU_NAME="${TPU_NAME:-tpu-v3-128-euw4a-53}"
+export TENSORFORK_RUN="tensorfork/tensorfork#27"
+printf '\033]2;%s\033\\' "bigrun61 ${TPU_NAME} @ ${TPU_HOST} (${TENSORFORK_RUN})" # set tmux title
+export MODEL_DIR="${MODEL_DIR:-gs://darnbooru-euw4a/runs/bigrun61chaos256/}"
+export NUM_CLASSES=1000
+export TPU_SPLIT_COMPILE_AND_EXECUTE=1
+export TF_TPU_WATCHDOG_TIMEOUT=1800
+while true; do
+  timeout --signal=SIGKILL 19h python3 wrapper.py compare_gan/main.py --use_tpu --tfds_data_dir 'gs://darnbooru-euw4a/tensorflow_datasets/' --model_dir "${MODEL_DIR}" --gin_config example_configs/biggan_danbooru256.gin "$@" 2>&1 | tee -a logs61.txt
+  sleep 30
+done
