dd5d25afe014d230d104442f1f5a71fe3de36220
Shawn Presser <shawnpresser@gmail.com>
2020-06-06 18:46:05 -0700

    Add run_danboorufigurespad256.sh

---
+++ run_danboorufigurespad256.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+set -ex
+export LD_LIBRARY_PATH="${LD_LIBRARY_PATH:-/tfk/lib}"
+#export TPU_HOST=10.255.128.3
+export TPU_HOST=${TPU_HOST:-10.255.128.2}
+export TPU_NAME="${TPU_NAME:-tpu-v3-256-euw4a-1}"
+tmux-set-title "bigrun83 ${TPU_NAME}"
+export MODEL_DIR="${MODEL_DIR:-gs://dota-euw4a/runs/bigrun83/}"
+export DATASETS=gs://dota-euw4a/datasets/danbooru2019figures/danbooru2019figures-0*
+export LABELS=""
+export NUM_CLASSES=1000
+export TPU_SPLIT_COMPILE_AND_EXECUTE=1
+export TF_TPU_WATCHDOG_TIMEOUT=1800
+while true; do
+  timeout --signal=SIGKILL 19h python3 wrapper.py compare_gan/main.py --use_tpu --tfds_data_dir 'gs://dota-euw4a/tensorflow_datasets/' --model_dir "${MODEL_DIR}" --gin_config example_configs/biggan_danboorufigurespad256.gin "$@" 2>&1 | tee -a logs83.txt
+  sleep 30
+done
+
