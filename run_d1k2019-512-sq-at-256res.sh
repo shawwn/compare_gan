9046f869f1eeb46b9a239fb20882914bf9935590
Shawn Presser <shawnpresser@gmail.com>
2020-06-12 07:02:33 -0700

    Fix

--- run_d1k2019-512-sq-at-256res.sh
+++ run_d1k2019-512-sq-at-256res.sh
@@ -12,7 +12,7 @@ export TPU_SPLIT_COMPILE_AND_EXECUTE=1
 export TF_TPU_WATCHDOG_TIMEOUT=1800
 while true; do
   timeout --signal=SIGKILL 19h python3 wrapper.py compare_gan/main.py --use_tpu --tfds_data_dir 'gs://dota-euw4a/tensorflow_datasets/' --model_dir "${MODEL_DIR}" --gin_config example_configs/biggan_d1k2019-512-sq-at-256res.gin "$@" 2>&1 | tee -a logs84.txt
-  if [ -z "$TPU_NO_RECREATE" ]
+  if [ ! -z "$TPU_NO_RECREATE" ]
   then
     echo "Not recreating TPU."
     sleep 30
--- run_danboorufigurespad256.sh
+++ run_danboorufigurespad256.sh
@@ -13,7 +13,7 @@ export TPU_SPLIT_COMPILE_AND_EXECUTE=1
 export TF_TPU_WATCHDOG_TIMEOUT=1800
 while true; do
   timeout --signal=SIGKILL 19h python3 wrapper.py compare_gan/main.py --use_tpu --tfds_data_dir 'gs://dota-euw4a/tensorflow_datasets/' --model_dir "${MODEL_DIR}" --gin_config example_configs/biggan_danboorufigurespad256.gin "$@" 2>&1 | tee -a logs83.txt
-  if [ -z "$TPU_NO_RECREATE" ]
+  if [ ! -z "$TPU_NO_RECREATE" ]
   then
     echo "Not recreating TPU."
     sleep 30
