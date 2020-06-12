d22c377d755de30ac3499591e735c3564fec2fb5
Shawn Presser <shawnpresser@gmail.com>
2020-06-12 06:48:20 -0700

    Recreate the TPU whenever there's an error during training.

--- run_danboorufigurespad256.sh
+++ run_danboorufigurespad256.sh
@@ -13,6 +13,15 @@ export TPU_SPLIT_COMPILE_AND_EXECUTE=1
 export TF_TPU_WATCHDOG_TIMEOUT=1800
 while true; do
   timeout --signal=SIGKILL 19h python3 wrapper.py compare_gan/main.py --use_tpu --tfds_data_dir 'gs://dota-euw4a/tensorflow_datasets/' --model_dir "${MODEL_DIR}" --gin_config example_configs/biggan_danboorufigurespad256.gin "$@" 2>&1 | tee -a logs83.txt
-  sleep 30
+  if [ -z "$TPU_NO_RECREATE" ]
+  then
+    echo "Not recreating TPU."
+    sleep 30
+  else
+    echo "Recreating TPU in 30s."
+    sleep 30
+    # sudo pip3 install -U tpudiepie
+    pu recreate "$TPU_NAME" --yes
+  fi
 done
 
