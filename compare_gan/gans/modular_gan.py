f2453b8b0814cccc3446776963522b2a0bb112aa
Shawn Presser <shawnpresser@gmail.com>
2020-06-28 22:50:36 -0700

    Remove training dependency on tfgan

--- compare_gan/gans/modular_gan.py
+++ compare_gan/gans/modular_gan.py
@@ -55,6 +55,55 @@ import contextlib
 FLAGS = flags.FLAGS
 
 
+def image_grid(input_tensor, grid_shape, image_shape=(32, 32), num_channels=3):
+  """Arrange a minibatch of images into a grid to form a single image.
+
+  Args:
+    input_tensor: Tensor. Minibatch of images to format, either 4D
+        ([batch size, height, width, num_channels]) or flattened
+        ([batch size, height * width * num_channels]).
+    grid_shape: Sequence of int. The shape of the image grid,
+        formatted as [grid_height, grid_width].
+    image_shape: Sequence of int. The shape of a single image,
+        formatted as [image_height, image_width].
+    num_channels: int. The number of channels in an image.
+
+  Returns:
+    Tensor representing a single image in which the input images have been
+    arranged into a grid.
+
+  Raises:
+    ValueError: The grid shape and minibatch size don't match, or the image
+        shape and number of channels are incompatible with the input tensor.
+  """
+  if grid_shape[0] * grid_shape[1] != int(input_tensor.shape[0]):
+    raise ValueError("Grid shape %s incompatible with minibatch size %i." %
+                     (grid_shape, int(input_tensor.shape[0])))
+  if len(input_tensor.shape) == 2:
+    num_features = image_shape[0] * image_shape[1] * num_channels
+    if int(input_tensor.shape[1]) != num_features:
+      raise ValueError("Image shape and number of channels incompatible with "
+                       "input tensor.")
+  elif len(input_tensor.shape) == 4:
+    if (int(input_tensor.shape[1]) != image_shape[0] or
+        int(input_tensor.shape[2]) != image_shape[1] or
+        int(input_tensor.shape[3]) != num_channels):
+      raise ValueError("Image shape and number of channels incompatible with "
+                       "input tensor. %s vs %s" % (
+                           input_tensor.shape, (image_shape[0], image_shape[1],
+                                                num_channels)))
+  else:
+    raise ValueError("Unrecognized input tensor format.")
+  height, width = grid_shape[0] * image_shape[0], grid_shape[1] * image_shape[1]
+  input_tensor = tf.reshape(input_tensor, tuple(grid_shape) + tuple(image_shape) + (num_channels,))
+  input_tensor = tf.transpose(a=input_tensor, perm=[0, 1, 3, 2, 4])
+  input_tensor = tf.reshape(input_tensor, [grid_shape[0], width, image_shape[0], num_channels])
+  input_tensor = tf.transpose(a=input_tensor, perm=[0, 2, 1, 3])
+  input_tensor = tf.reshape(input_tensor, [1, height, width, num_channels])
+  return input_tensor
+
+
+
 # pylint: disable=not-callable
 @gin.configurable(blacklist=["dataset", "parameters", "model_dir"])
 class ModularGAN(AbstractGAN):
@@ -362,7 +411,7 @@ class ModularGAN(AbstractGAN):
                         sample_shape[0], sample_shape[1])
         all_images = tf.image.resize(all_images, sample_shape[0:2], method=tf.image.ResizeMethod.AREA)
         shape = sample_shape
-      return tfgan.eval.image_grid(
+      return image_grid(
         all_images,
         grid_shape=grid_shape,
         image_shape=shape[:2],
