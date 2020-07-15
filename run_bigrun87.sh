19fa5b7c04be62e8ffe923e81d39f85c9123db51
Shawn Presser <shawnpresser@gmail.com>
2020-07-15 13:30:21 -0500

    Restart bigrun87

--- run_bigrun87.sh
+++ run_bigrun87.sh
@@ -6,7 +6,7 @@ export TPU_NAME="${TPU_NAME:-tpu-v3-256-euw4a-1}"
 
 export RUN_NAME="bigrun87"
 tmux-set-title "${RUN_NAME} ${TPU_NAME}"
-export MODEL_DIR="${MODEL_DIR:-gs://dota-euw4a/runs/bigrun87b/}"
+export MODEL_DIR="${MODEL_DIR:-gs://dota-euw4a/runs/bigrun87c/}"
 export GIN_CONFIG="example_configs/bigrun87.gin"
 
 date="$(python3 -c 'import datetime; print(datetime.datetime.now().strftime("%Y-%m-%d"))')"
