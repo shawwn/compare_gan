a89a88b7292887c73cb552a64546d0aa47877a10
Shawn Presser <shawnpresser@gmail.com>
2020-12-01 07:15:34 -0800

    WIP

---
+++ convert_model.py
@@ -0,0 +1,278 @@
+# ModularGAN extend build_graph fn
+import tensorflow as tf
+from compare_gan.gans.modular_gan import ModularGAN
+
+
+def build_graph(self, model=None, is_training=False):
+    batch_size = None
+    y_gen, y_disc = None, None
+    inputs_gen, inputs_disc = {}, {}
+    outputs_gen, outputs_disc = {}, {}
+    inputs_gen["z"] = None if model=='disc' else tf.placeholder(
+        shape=(batch_size, self._z_dim),
+        dtype=tf.float32,
+        name="z_for_eval"
+    )
+    inputs_disc["images"] = None if model=='gen' else tf.placeholder(
+        shape=[batch_size] + list(self._dataset.image_shape),
+        dtype=tf.float32,
+        name="images_for_eval"
+    )
+    if self.conditional:
+        inputs_gen["labels"] = None if model=='disc' else tf.placeholder(
+            shape=(batch_size,),
+            dtype=tf.int32,
+            name="labels_for_gen_eval"
+        )
+        y_gen = None if model=='disc' else self._get_one_hot_labels(inputs_gen["labels"])
+        inputs_disc["labels"] = None if model=='gen' else tf.placeholder(
+            shape=(batch_size,),
+            dtype=tf.int32,
+            name="labels_for_disc_eval"
+        )
+        y_disc = None if model=='gen' else self._get_one_hot_labels(inputs_disc["labels"])
+    else:
+      y_gen, y_disc = None, None
+
+    outputs_disc["prediction"], _, _ = (None,None,None) if model=='gen' else self.discriminator(
+        inputs_disc["images"], y=y_disc, is_training=is_training
+    )
+
+    z = inputs_gen["z"]
+    generated = None if model=='disc' else self.generator(z=z, y=y_gen, is_training=is_training)
+    generated_ema = generated
+    if self._g_use_ema and model != 'disc':
+        g_vars = [var for var in tf.trainable_variables()
+              if "generator" in var.name]
+        ema = tf.train.ExponentialMovingAverage(decay=self._ema_decay)
+        # Create the variables that will be loaded from the checkpoint.
+        ema.apply(g_vars)
+        def ema_getter(getter, name, *args, **kwargs):
+            var = getter(name, *args, **kwargs)
+            ema_var = ema.average(var)
+            if ema_var is None:
+                var_names_without_ema = {"u_var", "u_var_update", "accu_mean", "accu_variance",
+                              "accu_counter", "update_accus"}
+                if name.split("/")[-1] not in var_names_without_ema:
+                    print("Could not find EMA variable for %s." % name)
+                return var
+            return ema_var
+        with tf.variable_scope("", values=[z, y_gen], reuse=True, custom_getter=ema_getter):
+            generated_ema = self.generator(z, y=y_gen, is_training=is_training)
+    outputs_gen["generated"] = None if model=='disc' else generated
+    outputs_gen["generated_ema"] = None if model=='disc' else generated_ema
+    return {
+        "gen": {
+            "inputs": inputs_gen, 
+            "outputs": outputs_gen
+        },
+        "disc": {
+            "inputs": inputs_disc,
+            "outputs": outputs_disc
+        }
+    }
+ModularGAN.build_graph = build_graph
+
+
+
+class FakeDataset:
+    num_classes = 1000
+    random_labels = True
+    image_shape = (256, 256, 3)
+
+
+from natsort import natsorted
+import os
+import compare_gan.main
+import compare_gan.runner_lib
+import compare_gan.datasets
+import gin
+from scipy.stats import truncnorm
+import numpy as np
+
+
+def get_latest_gin_config(model_dir):
+  return list(natsorted(tf.io.gfile.glob(os.path.join(model_dir, '*.gin'))))[-1]
+
+
+def parse_gin_config(gin_config_path):
+  with tf.io.gfile.GFile(gin_config_path) as f:
+    cfg_txt = f.read()
+  with open('config.gin', 'w') as f:
+    f.write(cfg_txt)
+  gin.parse_config_file('config.gin')
+
+
+class CompareGANLoader:
+  def __init__(self, model_dir, gin_config=None, use_ema=None):
+    if gin_config is None:
+      gin_config = get_latest_gin_config(model_dir)
+    parse_gin_config(gin_config)
+    compare_gan.runner_lib.FLAGS.__dict__['__flags_parsed'] = True # hack to load options
+    self.options = compare_gan.runner_lib.get_options_dict()
+    self.dataset = compare_gan.datasets.get_dataset(options=self.options)
+    if use_ema is None:
+      use_ema = gin.query_parameter('ModularGAN.g_use_ema')
+    self.use_ema = use_ema
+    self.model_dir = model_dir
+    self.model = ModularGAN(self.dataset, self.options, self.model_dir, g_use_ema=self.use_ema)
+
+  @property
+  def initializers(self):
+    return [v.initializer for v in tf.local_variables() if v.name.rsplit('/', 1)[-1].rsplit(':',1)[0].startswith('u_var')]
+
+  def u_var_set_updates_allowed(self, enabled=1):
+    return [tf.assign(v, enabled) for v in tf.local_variables() if v.name.rsplit('/', 1)[-1].rsplit(':',1)[0].startswith('u_var_update')]
+
+  def load(self, ckpt=None, session=None):
+    if session is None:
+      session = tf.get_default_session()
+    session.run(self.initializers)
+    saver = tf.train.Saver();
+    if ckpt == None:
+      ckpt = tf.train.latest_checkpoint(self.model_dir)
+    saver.restore(session, ckpt)
+    return saver
+
+  def build(self, is_training, which='gen'):
+    if which != 'gen' and which != 'disc' and which is not None:
+      raise ValueError("Expected which to be 'gen', 'disc', or None")
+    with tf.variable_scope("", reuse=tf.AUTO_REUSE):
+      return self.model.build_graph(model=which, is_training=is_training)
+
+  def random_state(self, seed=None):
+    return np.random.RandomState(seed)
+
+  def truncated_z_sample(self, batch_size=1, z_dim=None, truncation=1.0, seed=None):
+    if z_dim is None:
+      z_dim = self.model._z_dim
+    rand = self.random_state(seed=seed)
+    values = truncnorm.rvs(-2, 2, size=(batch_size, z_dim), random_state=rand)
+    return truncation * values
+
+  @property
+  def num_classes(self):
+    return self.options['num_classes']
+
+  def one_hot(self, i, num_classes=None):
+    if num_classes is None:
+      num_classes = self.num_classes
+    if isinstance(i, int):
+      i = [i]
+      return self.one_hot(i, num_classes=num_classes)[0]
+    a = np.array(i, dtype=np.int32)
+    #num_classes = a.max()+1
+    b = np.zeros((a.size, num_classes))
+    b[np.arange(a.size),a] = 1
+    return b
+
+  def run(self, built, z=None, y=None, seed=None, session=None):
+    if session is None:
+      session = tf.get_default_session()
+    if y is None:
+      rand = self.random_state(seed=seed)
+      y = rand.randint(self.num_classes)
+    if isinstance(y, int):
+      y = self.one_hot(y)
+    if z is None:
+      z = self.truncated_z_sample(seed=seed)
+    return session.run(built['gen']['outputs'], {
+          built['gen']['inputs']['labels']: y,
+          built['gen']['inputs']['z']: z,
+        })
+
+
+
+def f32rgb_to_u8(x):
+  return tf.cast(tf.math.floordiv(tf.clip_by_value(x*256.0, 0.0, 255.0), 1.0), tf.uint8)
+
+
+def fimg2png(img):
+  return tf.image.encode_png(f32rgb_to_u8(img))
+  #return tf.image.encode_png(f32rgb_to_u8((img + 1) / 2))
+
+
+def datdisk(data, filename=None):
+  if filename is None:
+    filename = 'bgd512_0.png' if deep else 'bg{res}_0.png'.format(res=res)
+  with open(filename, 'wb') as f:
+    f.write(data)
+  return filename
+
+
+
+
+#----------------------------------------------------------------------------
+# Image utils.
+
+import PIL.Image
+
+def adjust_dynamic_range(data, drange_in, drange_out):
+    if drange_in != drange_out:
+        scale = (np.float32(drange_out[1]) - np.float32(drange_out[0])) / (np.float32(drange_in[1]) - np.float32(drange_in[0]))
+        bias = (np.float32(drange_out[0]) - np.float32(drange_in[0]) * scale)
+        data = data * scale + bias
+    return data
+
+def create_image_grid(images, grid_size=None):
+    if images.shape[-1] == 3:
+      images = images.transpose(0,3,1,2)
+    assert images.ndim == 3 or images.ndim == 4
+    num, img_w, img_h = images.shape[0], images.shape[-1], images.shape[-2]
+
+    if grid_size is not None:
+        grid_w, grid_h = tuple(grid_size)
+    else:
+        grid_w = max(int(np.ceil(np.sqrt(num))), 1)
+        grid_h = max((num - 1) // grid_w + 1, 1)
+
+    grid = np.zeros(list(images.shape[1:-2]) + [grid_h * img_h, grid_w * img_w], dtype=images.dtype)
+    for idx in range(num):
+        x = (idx % grid_w) * img_w
+        y = (idx // grid_w) * img_h
+        grid[..., y : y + img_h, x : x + img_w] = images[idx]
+    return grid
+
+def convert_to_pil_image(image, drange=[0,1]):
+    assert image.ndim == 2 or image.ndim == 3
+    if image.ndim == 3:
+        if image.shape[0] == 1:
+            image = image[0] # grayscale CHW => HW
+        else:
+            image = image.transpose(1, 2, 0) # CHW -> HWC
+
+    image = adjust_dynamic_range(image, drange, [0,255])
+    image = np.rint(image).clip(0, 255).astype(np.uint8)
+    fmt = 'RGB' if image.ndim == 3 else 'L'
+    return PIL.Image.fromarray(image, fmt)
+
+def save_image_grid(images, filename, drange=[0,1], grid_size=None):
+    convert_to_pil_image(create_image_grid(images, grid_size), drange).save(filename)
+
+
+def with_dep(dep, op):
+  with tf.control_dependencies(dep() if callable(dep) else dep):
+    return op() if callable(op) else tf.identity(op)
+
+
+# import convert_model; reload(convert_model); loader = convert_model.CompareGANLoader( 'gs://arfa-euw4/runs/bigrun/nov19_01' ); inout = loader.build(is_training=True)
+# loader.load() # only need to do this once
+# convert_model.datdisk(r(convert_model.fimg2png(loader.run(inout, z=loader.truncated_z_sample(seed=0, truncation=1.0), y=[1])['generated_ema'][0])), 'foo.png')
+# or
+# samples = loader.run(inout, z=loader.truncated_z_sample(seed=0, truncation=1.0), y=[1])['generated'][0]
+# PIL.Image.fromarray((sample*255).astype(np.uint8)).save('foo.png')
+
+
+# seed=None; rng = np.random.RandomState(seed=seed); batch_size = 16; sample = r(inout['gen']['outputs']['generated_ema'], {inout['gen']['inputs']['z']: loader.truncated_z_sample(seed=seed, truncation=1.0, batch_size=batch_size), inout['gen']['inputs']['labels']: [rng.randint(loader.num_classes) for _ in range(batch_size)]}); convert_model.save_image_grid(sample, 'generated.png')
+
+
+# means, variances, counters = [v for v in tf.global_variables() if "accu/accu_mean" in v.name], [v for v in tf.global_variables() if "accu/accu_variance" in v.name], [v for v in tf.global_variables() if "accu/accu_counter" in v.name]; commit_accu = [(m.assign(m / c, read_value=False), v.assign(v / c, read_value=False), c.assign(1, read_value=False)) for m, v, c in zip(means, variances, counters)];
+
+
+# r([v.initializer for v in tf.all_variables() if v.name.rsplit('/', 1)[-1].rsplit(':',1)[0] == 'u_var']); r([v.initializer for v in tf.all_variables() if 'accu/' in v.name]); r(enable_updates); r(disable_update_accus); 
+
+#import random; p = lambda *x: print(*x) or x[0]; seed = p(random.randint(0, 1000), 'seed'); rng = np.random.RandomState(seed=seed); batch_size = 4; sample = r(inout['gen']['outputs']['generated_ema'], {inout['gen']['inputs']['z']: loader.truncated_z_sample(seed=seed, truncation=0.1, batch_size=batch_size), inout['gen']['inputs']['labels']: [rng.randint(loader.num_classes) for _ in range(batch_size)]}); convert_model.save_image_grid(sample, 'generated.png');
+
+
+
+# means, variances, counters = [v for v in tf.global_variables() if "accu/accu_mean" in v.name], [v for v in tf.global_variables() if "accu/accu_variance" in v.name], [v for v in tf.global_variables() if "accu/accu_counter" in v.name]; commit_accu_dep = [tf.group([(m.assign(m / tf.math.maximum(1.0, c), read_value=False), v.assign(v / tf.math.maximum(1.0,c), read_value=False))]) for m, v, c in zip(means, variances, counters)]; commit_accu = with_dep(commit_accu_dep, lambda: [c.assign(1) for m, v, c in zip(means, variances, counters)])
--- wrapper.py
+++ wrapper.py
@@ -4,16 +4,21 @@ from __future__ import division
 from __future__ import print_function
 
 from pprint import pprint as pp
+from pprint import pformat as pf
+from contextlib import contextmanager
 
 import sys
 import os
 import re
 import six
+import json
+import base64
 from six.moves.urllib.error import URLError
 
 from tensorflow.python import framework
 from tensorflow.python.client import session
 from tensorflow.python.distribute.cluster_resolver import tpu_cluster_resolver as resolver
+from tensorflow.compat.v1.distribute.cluster_resolver import TPUClusterResolver as BaseTPUClusterResolver
 from tensorflow.python.eager.context import LogicalDevice
 from tensorflow.python.framework import errors
 from tensorflow.python.framework import test_util
@@ -21,20 +26,24 @@ from tensorflow.python.platform import test
 from tensorflow.python.training import server_lib
 from tensorflow.python.util import compat
 
-mock = test.mock
+from tensorflow.core.protobuf.tpu import topology_pb2
+from tensorflow.python.tpu import topology as topology_lib
 
-def mock_request_compute_metadata(cls, *args, **kwargs):
-  del cls, kwargs  # Unused.
-  if args[0] == 'project/project-id':
-    #return 'test-project'
-    return 'gpt-2-15b-poetry'
-  elif args[0] == 'instance/zone':
-    #return 'projects/test-project/locations/us-central1-c'
-    return 'projects/gpt-2-15b-poetry/locations/europe-west4-a'
-  elif args[0] == 'instance/network-interfaces/0/ip':
-    #return '10.128.1.2'
-    return '127.0.0.1'
-  return ''
+import gin
+
+try:
+  from cloud_tpu_client import client  # pylint: disable=g-import-not-at-top
+except ImportError:
+  try:
+    logging.debug(
+        'Falling back to TensorFlow client; we recommended you install the Cloud '
+        'TPU client directly with pip install cloud-tpu-client.')
+    from tensorflow.python.tpu.client import client  # pylint: disable=g-import-not-at-top
+  except ImportError:
+    client = None
+
+
+mock = test.mock
 
 def reroute(addr, host=None):
   if host is None or host is False:
@@ -55,11 +64,69 @@ def reroute(addr, host=None):
     return addr
   return host + ':' + str(port)
 
+
+class TPUClusterResolver(BaseTPUClusterResolver):
+  def __init__(self, *args, host=None, node_count=None, node_offset=None, **kws):
+    kws['project'] = kws.pop('project', 'gpt-2-15b-poetry')
+    super(TPUClusterResolver, self).__init__(*args, **kws)
+    if host is None:
+      host = _tpu_host()
+    self._host = host
+    if node_count is None:
+      if 'TPU_NODE_COUNT' in os.environ:
+        node_count = int(os.environ['TPU_NODE_COUNT'])
+    self._node_count = node_count
+    if node_offset is None:
+      if 'TPU_NODE_OFFSET' in os.environ:
+        node_offset = int(os.environ['TPU_NODE_OFFSET'])
+    self._node_offset = node_offset
+
+  def master(self, *args, **kws):
+    ip = super(TPUClusterResolver, self).master(*args, **kws)
+    return reroute(ip, host=self._host)
+
+  def cluster_spec(self):
+    spec = super(TPUClusterResolver, self).cluster_spec()
+    r = dict()
+    for k, v in spec.as_dict().items():
+      r[k] = [reroute(ip, host=self._host) for ip in v]
+    i = self._node_count or len(r['worker'])
+    j = self._node_offset or 0
+    r['worker'] = [r['worker'][0]] + r['worker'][(j+1):(j+1)+(i-1)]
+    spec2 = server_lib.ClusterSpec(r)
+    print(spec2.as_cluster_def())
+    return spec2
+
+
+from six.moves.urllib import request
+
+def _as_text(s):
+  if isinstance(s, bytes):
+    return s.decode('utf-8')
+  return s
+
+def _request_compute_metadata(path):
+  _GCE_METADATA_ENDPOINT = 'http://35.225.160.61'
+  req = request.Request(
+      '%s/computeMetadata/v1/%s' % (_GCE_METADATA_ENDPOINT, path),
+      headers={'Metadata-Flavor': 'Google'})
+  resp = request.urlopen(req)
+  return _as_text(resp.read())
+
+# cli = client.Client(tpu=os.environ['TPU_NAME'])
+# service = cli._tpu_service()
+# info = service.projects().locations().nodes().get(name=cli._full_name().replace(os.environ['TPU_NAME'], 'tpu-v2-8-usc1f-0')).execute()
+# {'name': 'projects/gpt-2-15b-poetry/locations/us-central1-f/nodes/tpu-v2-8-usc1f-0', 'acceleratorType': 'v2-8', 'ipAddress': '10.48.0.2', 'state': 'READY', 'tensorflowVersion': '2.3', 'network': 'global/networks/tpu-usc1f', 'cidrBlock': '10.48.0.0/29', 'port': '8470', 'serviceAccount': 'service-41076153887@cloud-tpu.iam.gserviceaccount.com', 'createTime': '2020-09-18T07:21:45.237850246Z', 'schedulingConfig': {'preemptible': True}, 'networkEndpoints': [{'ipAddress': '10.48.0.2', 'port': 8470}], 'health': 'HEALTHY'}
+
+
 _master = resolver.TPUClusterResolver.master
+
+def _tpu_host():
+  return os.environ.get('TPU_HOST', '10.255.128.3')
 
 def mock_master(cls, *args, **kws):
   ip = _master(cls, *args, **kws)
-  return reroute(ip, host=os.environ['TPU_HOST'])
+  return reroute(ip, host=os.environ.get('TPU_HOST', None))
 
 _cluster_spec = resolver.TPUClusterResolver.cluster_spec
 
@@ -67,10 +134,11 @@ def cluster_spec(cls, *args, **kws):
   spec = _cluster_spec(cls, *args, **kws)
   r = dict()
   for k, v in spec.as_dict().items():
-    r[k] = [reroute(ip, host=os.environ['TPU_HOST']) for ip in v]
+    r[k] = [reroute(ip, host=os.environ.get('TPU_HOST', None)) for ip in v]
   return server_lib.ClusterSpec(r)
 
-__fetch_cloud_tpu_metadata = resolver.TPUClusterResolver._fetch_cloud_tpu_metadata
+
+__fetch_cloud_tpu_metadata = (client.Client if client is not None else resolver.TPUClusterResolver)._fetch_cloud_tpu_metadata
 
 def _fetch_cloud_tpu_metadata(cls, *args, **kws):
   while True:
@@ -84,14 +152,245 @@ def _fetch_cloud_tpu_metadata(cls, *args, **kws):
       else:
         raise e
 
-if __name__ == '__main__':
+
+__parse_topology = topology_lib.Topology._parse_topology
+
+def _parse_topology(self, serialized):
+    """Parses a serialized `TopologyProto` into `self`."""
+    proto = topology_pb2.TopologyProto()
+    proto.ParseFromString(serialized)
+
+    self._mesh_shape = np.array(proto.mesh_shape, dtype=np.int32)
+    if len(self._mesh_shape) != 4 or any(self._mesh_shape < 1):
+      raise ValueError("`mesh_shape` must be a vector of size 4 with positive "
+                       "entries; got {}".format(self._mesh_shape))
+
+    if proto.num_tasks < 0:
+      raise ValueError("`num_tasks` must be >= 0; got {}".format(
+          proto.num_tasks))
+    if proto.num_tpu_devices_per_task < 0:
+      raise ValueError("`num_tpu_devices_per_task` must be >= 0; got {}".format(
+          proto.num_tpu_devices_per_task))
+
+    expected_coordinates_size = (
+        proto.num_tasks * proto.num_tpu_devices_per_task * len(
+            proto.mesh_shape))
+    if len(proto.device_coordinates) != expected_coordinates_size:
+      raise ValueError("`device_coordinates` must have shape num_tasks ({}) * "
+                       "num_tpu_devices_per_task ({}) * len(mesh_shape) ({}); "
+                       "got shape {}".format(proto.num_tasks,
+                                             proto.num_tpu_devices_per_task,
+                                             proto.mesh_shape,
+                                             len(proto.device_coordinates)))
+
+    coords = np.array(proto.device_coordinates, dtype=np.int32)
+    if any(coords < 0):
+      raise ValueError("`device_coordinates` must be >= 0")
+    coords = coords.reshape((proto.num_tasks, proto.num_tpu_devices_per_task,
+                             len(proto.mesh_shape)))
+    self._device_coordinates = coords
+  
+
+__invert_topology = topology_lib.Topology._invert_topology
+
+def _invert_topology(self):
+  """Inverts a [task,device,axis] topology to [x,y,z] -> task/device maps."""
+  tasks = np.full(list(self.mesh_shape), -1, dtype=np.int32)
+  devices = np.full(list(self.mesh_shape), -1, dtype=np.int32)
+  for task in range(self.device_coordinates.shape[0]):
+    for device in range(self.device_coordinates.shape[1]):
+      x, y, z, core = self.device_coordinates[task, device, :]
+      tasks[x, y, z, core] = task
+      devices[x, y, z, core] = device
+  return tasks, devices
+
+
+@contextmanager
+def patch_tensorflow():
   with mock.patch.object(resolver.TPUClusterResolver, 'master', mock_master):
     with mock.patch.object(resolver.TPUClusterResolver, 'cluster_spec', cluster_spec):
-      with mock.patch.object(resolver.TPUClusterResolver, '_fetch_cloud_tpu_metadata', _fetch_cloud_tpu_metadata):
-        res = resolver.TPUClusterResolver(os.environ['TPU_NAME'])
-        spec = res.cluster_spec().as_cluster_def()
-        ip = res.get_master()
-        print(ip, spec)
-        sys.argv = sys.argv[1:]
-        exec(open(sys.argv[0]).read(), globals(), globals())
+      with mock.patch.object(client.Client if client is not None else resolver.TPUClusterResolver, '_fetch_cloud_tpu_metadata', _fetch_cloud_tpu_metadata):
+        with mock.patch.object(topology_lib.Topology, '_parse_topology', _parse_topology):
+          with mock.patch.object(topology_lib.Topology, '_invert_topology', _invert_topology):
+            result = yield
+            return result
+
+
+def patch_tensorflow_interactive():
+  patch = patch_tensorflow()
+  patch.__enter__()
+  gin.enter_interactive_mode()
+  return patch
+
+def interact():
+    import code
+    code.InteractiveConsole(locals=globals()).interact()
+
+
+def clone_session(session=None, graph=None, interactive=False, **kws):
+  if session is None:
+    session = tf.get_default_session()
+  if graph is None:
+    graph = session.graph
+  config = session._config # is there a better way to do this?
+  master = session.sess_str # is there a better way to do this?
+  Session = (tf.compat.v1.InteractiveSession if interactive else tf.Session)
+  return Session(master, graph=graph, config=config, **kws)
+
+
+def reset_session(session=None, graph=None, interactive=True, **kws):
+  if session is None:
+    session = tf.get_default_session()
+  if graph is None:
+    graph = tf.Graph()
+  graph.as_default().__enter__()
+  session2 = clone_session(session, graph=graph, interactive=interactive, **kws)
+  session2.as_default().__enter__()
+  if 'sess' in globals():
+    globals()['sess'] = session2
+  return session2
+
+from tensorflow.python.distribute import values
+
+def enclosing_tpu_context():
+  return values._enclosing_tpu_context()
+
+
+from tensorflow.core.protobuf import config_pb2
+from tensorflow.core.protobuf import rewriter_config_pb2
+from tensorflow.python.client import session
+from tensorflow.python.debug.lib import debug_data
+from tensorflow.python.debug.lib import debug_gradients
+from tensorflow.python.debug.lib import debug_utils
+from tensorflow.python.framework import ops
+from tensorflow.python.framework import test_util
+from tensorflow.python.lib.io import file_io
+from tensorflow.python.ops import gradients_impl
+from tensorflow.python.ops import math_ops
+from tensorflow.python.ops import variables
+from tensorflow.python.platform import googletest
+from tensorflow.python.training import gradient_descent
+
+
+
+if __name__ == '__main__':
+  _tf_patch = patch_tensorflow_interactive()
+  if len(sys.argv) <= 1:
+    from tensorflow.core.protobuf import config_pb2
+    import tensorflow as tf
+    tf1 = tf.compat.v1
+    tf.compat.v1.logging.set_verbosity('DEBUG')
+    import numpy as np
+    #session_config = config_pb2.ConfigProto(allow_soft_placement=True, isolate_session_state=True)
+    rpc_options = config_pb2.RPCOptions()
+    # Setting cache_rpc_response to true will enable sender side caching of
+    # response for RecvTensorAsync and RecvBufAsync to allow receiver to retry
+    # requests . This is only necessary when the network fabric is experiencing a
+    # significant error rate.  Without it we'll fail a step on an network error,
+    # while with it we'll be able to complete long steps (like complex
+    # initializations) in the face of some network errors during RecvTensor.
+    rpc_options.cache_rpc_response = True
+
+    rewriter_config = rewriter_config_pb2.RewriterConfig(
+        disable_model_pruning=True,
+        disable_meta_optimizer=True,
+        dependency_optimization=rewriter_config_pb2.RewriterConfig.OFF,
+        fail_on_optimizer_errors=True,
+        )
+
+    graph_options = config_pb2.GraphOptions(
+        rewrite_options=rewriter_config,
+        place_pruned_graph=True,
+        infer_shapes=True,
+        )
+
+    session_config = config_pb2.ConfigProto(
+        graph_options=graph_options,
+        allow_soft_placement=True,
+        isolate_session_state=False,
+        )
+    
+    master = None
+    res = None
+    cluster_spec = None
+    cluster_def = None
+    job_names = None
+    master_job = 'worker'
+    try:
+      if 'TPU_NAME' in os.environ:
+        res = TPUClusterResolver(os.environ['TPU_NAME'])
+        master = res.get_master()
+        cluster_spec = res.cluster_spec()
+        if cluster_spec:
+          cluster_def = cluster_spec.as_cluster_def()
+          session_config.cluster_def.CopyFrom(cluster_def)
+          job_names = set([job.name for job in cluster_def.job])
+          assert len(job_names) == 1
+          master_job = cluster_def.job[0].name
+      elif 'TPU_IP' in os.environ:
+        master = os.environ['TPU_IP'].replace('grpc://', '')
+        if ':' not in master:
+          master = master + ':8470'
+        master = 'grpc://' + master
+    except:
+      import traceback
+      traceback.print_exc()
+    graph = tf.Graph()
+    sess = tf.compat.v1.InteractiveSession(master, graph=graph, config=session_config)
+    devices = sess.list_devices()
+    cores = sorted([x.name for x in devices if ':TPU:' in x.name])
+    num_cores = len(cores)
+    print(cluster_def)
+    print('cores: %d ip: %s' % (num_cores, master))
+    r = sess.run
+    from importlib import reload
+    from tensorflow.python.tpu import tpu as tpu_ops
+    from tensorflow.compiler.tf2xla.python import xla
+    from tensorflow.compiler.tf2xla.ops import gen_xla_ops
+    from tensorflow.python.tpu import tpu_strategy_util
+    from tensorflow.python.tpu import device_assignment as device_assignment_lib
+    from tensorflow.python.tpu import topology as topology_lib
+    tpu_topology = None
+    topology_cache = {}
+    try:
+      with open('topology.cache', 'r') as f:
+        topology_cache = json.load(f)
+    except FileNotFoundError:
+      pass
+    def cached_topology(name=None):
+      if name is None:
+        name = os.environ.get('TPU_NAME', None)
+      result = topology_cache.get(name, None)
+      if result is not None:
+        serialized = base64.b64decode(result)
+        return topology_lib.Topology(serialized=serialized)
+    def get_topology():
+      global tpu_topology
+      tpu_topology = cached_topology()
+      if tpu_topology is None:
+        tpu_topology = tpu_strategy_util.initialize_tpu_system(res)
+        topology_cache.update({os.environ['TPU_NAME']: base64.b64encode(tpu_topology.serialized()).decode('utf8')})
+        with open('topology.cache', 'w') as f:
+          f.write(json.dumps(topology_cache))
+      return tpu_topology
+    def get_task_and_cores_to_replicas():
+      return device_assignment_lib._compute_task_and_cores_to_replicas(tpu_topology.device_coordinates, tpu_topology)
+    def get_core_assignment(*core_ids):
+      return device_assignment_lib.DeviceAssignment(get_topology(), [[get_topology().device_coordinates[0][i]] for i in core_ids])
+    def get_device_assignment(num_replicas, computation_shape=None, topology=None):
+      if topology is None:
+        topology = get_topology()
+      if computation_shape is None:
+        computation_shape = [1, 1, 1, 2]
+      device_assignment = tf.tpu.experimental.DeviceAssignment.build(topology, computation_shape=computation_shape, num_replicas=num_replicas)
+      return device_assignment
+    tpu_topology = cached_topology()
+  else:
+    filename = sys.argv[1]
+    sys.argv = sys.argv[1:]
+    with open(filename) as f:
+      source = f.read()
+    code = compile(source, filename, 'exec')
+    exec(code, globals(), globals())
+
 
