c6730f649a40e99be4d1ffd054cc219a73b271a0
Shawn Presser <shawnpresser@gmail.com>
2020-12-16 23:50:22 -0800

    Graph specnorms statelessly

--- compare_gan/architectures/arch_ops.py
+++ compare_gan/architectures/arch_ops.py
@@ -577,6 +577,83 @@ def layer_norm(input_, is_training, scope):
   return tf.contrib.layers.layer_norm(
       input_, trainable=is_training, scope=scope)
 
+def spectral_norm_stateless(inputs, epsilon=1e-12, singular_value="right",
+                  power_iteration_rounds=20):
+  """Performs Spectral Normalization on a weight tensor.
+
+  Details of why this is helpful for GAN's can be found in "Spectral
+  Normalization for Generative Adversarial Networks", Miyato T. et al., 2018.
+  [https://arxiv.org/abs/1802.05957].
+
+  Args:
+    inputs: The weight tensor to normalize.
+    epsilon: Epsilon for L2 normalization.
+    singular_value: Which first singular value to store (left or right). Use
+      "auto" to automatically choose the one that has fewer dimensions.
+
+  Returns:
+    The normalized weight tensor.
+  """
+  if len(inputs.shape) <= 0:
+    logging.info("[ops] spectral norm of a float is itself; returning as-is. name=%s %s", inputs.name, repr(inputs))
+    return inputs, inputs
+
+  if is_training is None:
+    # we infer whether we're training or inferencing based on whether
+    # we're sampling from the EMA model or not.
+    current_scope = gin.current_scope_str()
+    if re.search(r"\bema\b", current_scope):
+      is_training = False
+    else:
+      is_training = True
+    logging.info("[ops] %s is_training=%s", current_scope, is_training)
+
+  # The paper says to flatten convnet kernel weights from (C_out, C_in, KH, KW)
+  # to (C_out, C_in * KH * KW). Our Conv2D kernel shape is (KH, KW, C_in, C_out)
+  # so it should be reshaped to (KH * KW * C_in, C_out), and similarly for other
+  # layers that put output channels as last dimension. This implies that w
+  # here is equivalent to w.T in the paper.
+  w = tf.reshape(inputs, (-1, inputs.shape[-1]))
+
+  # Choose whether to persist the first left or first right singular vector.
+  # As the underlying matrix is PSD, this should be equivalent, but in practice
+  # the shape of the persisted vector is different. Here one can choose whether
+  # to maintain the left or right one, or pick the one which has the smaller
+  # dimension. We use the same variable for the singular vector if we switch
+  # from normal weights to EMA weights.
+  if singular_value == "auto":
+    singular_value = "left" if w.shape[0] <= w.shape[1] else "right"
+  u_shape = (w.shape[0], 1) if singular_value == "left" else (1, w.shape[-1])
+  u = tf.random.normal(shape=u_shape, name='u0')
+
+  # Use power iteration method to approximate the spectral norm.
+  # The authors suggest that one round of power iteration was sufficient in the
+  # actual experiment to achieve satisfactory performance.
+  for _ in range(power_iteration_rounds):
+    if singular_value == "left":
+      # `v` approximates the first right singular vector of matrix `w`.
+      v = tf.math.l2_normalize(
+          tf.matmul(tf.transpose(w), u), axis=None, epsilon=epsilon)
+      u = tf.math.l2_normalize(tf.matmul(w, v), axis=None, epsilon=epsilon)
+    else:
+      v = tf.math.l2_normalize(tf.matmul(u, w, transpose_b=True),
+                               epsilon=epsilon)
+      u = tf.math.l2_normalize(tf.matmul(v, w), epsilon=epsilon)
+
+  # The authors of SN-GAN chose to stop gradient propagating through u and v
+  # and we maintain that option.
+  u = tf.stop_gradient(u)
+  v = tf.stop_gradient(v)
+
+  if singular_value == "left":
+    norm_value = tf.matmul(tf.matmul(tf.transpose(u), w), v)
+  else:
+    norm_value = tf.matmul(tf.matmul(v, w), u, transpose_b=True)
+  norm_value.shape.assert_is_fully_defined()
+  norm_value.shape.assert_is_compatible_with([1, 1])
+  return norm_value[0][0]
+
+
 
 @gin.configurable(blacklist=["inputs"])
 def spectral_norm(inputs, epsilon=1e-12, singular_value="auto", use_resource=True,
@@ -716,8 +793,10 @@ def graph_spectral_norm(w, init=None):
   assert tpu_summaries.TpuSummaries.inst is not None
   if name is not None and not tpu_summaries.TpuSummaries.inst.has(name):
     logging.info("[ops] Graphing name=%s (was %s), %s", name, w.name, repr(w))
-    w1, norm = spectral_norm(w)
-    tpu_summaries.TpuSummaries.inst.scalar(name, norm, countdown=2, init=init) # the specnorm needs a few iters to become accurate
+    #w1, norm = spectral_norm(w)
+    #tpu_summaries.TpuSummaries.inst.scalar(name, norm, countdown=2, init=init) # the specnorm needs a few iters to become accurate
+    norm = spectral_norm_stateless(w)
+    tpu_summaries.TpuSummaries.inst.scalar(name, norm, init=init)
   else:
     logging.info("[ops] Not graphing %s", w.name)
   return w
