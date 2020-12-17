f7e0c0c2a1709bd90195730954759d9ace7d4f38
Shawn Presser <shawnpresser@gmail.com>
2020-12-17 15:04:08 -0800

    Abandon deep512 for now in favor of smaller deep256

--- run_bigrun94_big128deep512ch128_run1.sh
+++ run_bigrun94_big128deep512ch128_run1.sh
@@ -5,9 +5,9 @@ set -ex
 unset TPU_HOST
 export TPU_NAME="${TPU_NAME:-tpu-v3-256-euw4a-55}"
 
-export RUN_NAME="${RUN_NAME:-bigrun94_big128deep512ch128}"
+export RUN_NAME="${RUN_NAME:-bigrun94_big128deep256ch96}"
 tmux-set-title "${RUN_NAME} ${TPU_NAME}"
-export MODEL_DIR="${MODEL_DIR:-gs://doto-euw4a/runs/bigrun94_big128/dec17/deep512ch128/run2}"
+export MODEL_DIR="${MODEL_DIR:-gs://doto-euw4a/runs/bigrun94_big128/dec17/deep256ch96/run1}"
 export GIN_CONFIG="example_configs/bigrun94_big128deep512ch128.gin"
 
 date="$(python3 -c 'import datetime; print(datetime.datetime.now().strftime("%Y-%m-%d"))')"
