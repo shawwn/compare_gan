9df98b80eafe6edda852e6390b009e7acb9ee417
Shawn Presser <shawnpresser@gmail.com>
2020-12-17 04:40:31 -0800

    Add bigrun94_big128c

---
+++ run_bigrun94_big128c_run1.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+set -ex
+#export LD_LIBRARY_PATH="${LD_LIBRARY_PATH:-/tfk/lib}"
+#export TPU_HOST=${TPU_HOST:-10.255.128.3}
+unset TPU_HOST
+export TPU_NAME="${TPU_NAME:-tpu-v3-8-euw4a-4}"
+
+export RUN_NAME="${RUN_NAME:-bigrun94_big128c}"
+tmux-set-title "${RUN_NAME} ${TPU_NAME}"
+export MODEL_DIR="${MODEL_DIR:-gs://doto-euw4a/runs/bigrun94_big128/v3_8/c/run1}"
+export GIN_CONFIG="example_configs/bigrun94_big128c.gin"
+
+date="$(python3 -c 'import datetime; print(datetime.datetime.now().strftime("%Y-%m-%d"))')"
+logfile="logs/${RUN_NAME}-${date}.txt"
+mkdir -p logs
+
+export LABELS=""
+export NUM_CLASSES=1000
+export TPU_SPLIT_COMPILE_AND_EXECUTE=1
+export TF_TPU_WATCHDOG_TIMEOUT=1800
+#wrapper=wrapper.py 
+
+
+while true; do
+  timeout --signal=SIGKILL 8h python3 $wrapper compare_gan/main.py --use_tpu --tfds_data_dir 'gs://dota-euw4a/tensorflow_datasets/' --model_dir "${MODEL_DIR}" --gin_config "$GIN_CONFIG" --gin_bindings "begin_run.model_dir = '${MODEL_DIR}/'" --gin_bindings "begin_run.tpu_name = '${TPU_NAME}'" "$@" 2>&1 | tee -a "$logfile"
+  if [ ! -z "$TPU_NO_RECREATE" ]
+  then
+    echo "Not recreating TPU."
+    sleep 30
+  else
+    echo "Recreating TPU in 30s."
+    sleep 30
+    # sudo pip3 install -U tpudiepie
+    pu recreate "$TPU_NAME" --yes
+  fi
+done
